c2aa0327fd12805f017dcd565fbd44309683ee83
MarcOutt <marc.outteryck@gmail.com>
2023-04-11 11:27:59 +0200

    add model user and admin user

---
+++ src/user/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from user.models import CustomUser
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from user.models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email', 'first_name', 'last_name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_admin', 'role')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'role', 'is_admin', 'is_staff'),
+        }),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    filter_horizontal = ()
+    list_filter = ()
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ src/user/models.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError("Vous devez entrer un email")
+        if not first_name:
+            raise ValueError("Vous devez entrer un prénom")
+        if not last_name:
+            raise ValueError("Vous devez entrer un nom")
+
+        user = self.model(email=self.normalize_email(email), first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None):
+        user = self.create_user(email=self.normalize_email(email), first_name=first_name, last_name=last_name,
+                                password=password)
+        user.is_admin = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+
+    choice_role = [
+        ('management', 'management'),
+        ('support', 'support'),
+        ('sale', 'sale')
+    ]
+
+    email = models.EmailField(unique=True, max_length=255, blank=False)
+    first_name = models.CharField(max_length=125)
+    last_name = models.CharField(max_length=125)
+    role = models.CharField(max_length=50, choices=choice_role, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = MyUserManager()
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Utilisateur"
+
